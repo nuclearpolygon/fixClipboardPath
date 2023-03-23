a005efbbe31125af420028c8df9c748b6da38a54
mrmcdobrotny <mr.prudov2012@gmail.com>
2023-03-23 13:57:35 +0300

    fix win installation

--- install.bat
+++ install.bat
@@ -1,8 +1,8 @@
 @echo off
 SET root_dir=%~dp0
 rem echo %root_dir%wnd\lesta_fixClipboard.exe
-erase "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\Startup\lesta_fixClipboard.exe" 
-mklink "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\Startup\lesta_fixClipboard.exe" "%root_dir%wnd\lesta_fixClipboard.exe" 
+erase "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\lesta_fixClipboard.exe" 
+copy "%root_dir%wnd\lesta_fixClipboard.exe" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\lesta_fixClipboard.exe"
 start /b cmd /c "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\Startup\lesta_fixClipboard.exe"
 rem echo "symlink created: %PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\Startup\lesta_fixClipboard.exe"
 echo installation completed
