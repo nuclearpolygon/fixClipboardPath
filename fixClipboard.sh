83ee18f109a1aa6320bc2bb9812a2e5864a2f214
n_prudov <mr.prudov2012@gmail.com>
2023-03-24 18:34:00 +0300

    fixed linux installation

--- fixClipboard.sh
+++ fixClipboard.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 script_dir=$( dirname -- "${BASH_SOURCE[0]}")
-#source $script_dir/venv/bin/activate
+source $script_dir/venv/bin/activate
 python $script_dir/main.py
--- install.sh
+++ install.sh
@@ -2,8 +2,16 @@
 p=$( dirname -- "${BASH_SOURCE[0]}")
 script_dir="$(cd "$(dirname "$p")"; pwd)/$(basename "$p")"
 pip install -r "${script_dir}/requirements.txt"
-file="[Unit]\nDescription=Fix Path Service\nAfter=multi-user.target\n[Service]\nType=simple\nRestart=always\nExecStart=${script_dir}/fixClipboard.sh\n[Install]\nWantedBy=multi-user.target"
-mkdir -p ~/.config/systemd/user/
-echo -e $file > ~/.config/systemd/user/fixclipboard.service
-systemctl --user enable fixclipboard
+file='[Unit]
+Description=Fix Path Service
+After=default.target
+[Service]
+Type=simple
+Restart=always
+ExecStart='$script_dir'/fixClipboard.sh
+[Install]
+WantedBy=default.target'
+[[ -d ~/.config/systemd/user ]] || ( mkdir -p ~/.config/systemd/user && echo 'dir created: ~/.config/systemd/user' )
+echo "$file" > ~/.config/systemd/user/fixclipboard.service && echo 'service created: fixclipboard.service'
+systemctl --user enable fixclipboard && echo 'service enabled'
 systemctl --user start fixclipboard
